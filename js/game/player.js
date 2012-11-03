fb47b75623681403395827a7d80a040fb8c3dbee
Keith Johnson <kj@ubergeek42.com>
2012-11-03 17:48:00 -0400

    Add player object with colors for fill

--- js/game/player.js
+++ js/game/player.js
@@ -1,9 +1,14 @@
 define(function(require) {
     var Entity = require('flux/entity');
+    var TiledGraphic = require('flux/graphics/tiled');
 
+    var loader = require('game/loader');
     function Player(x, y) {
         Entity.call(this, x, y);
         this.col = 0;
+        
+        this.playertiles = new TiledGraphic(loader.get('player'),16,24);
+        this.currentSprite = 0;
 
         this.keydown_delay = 0;
         this.keyheld = false;
@@ -39,6 +44,12 @@ define(function(require) {
         }
         if (!kb.check(kb.D)){
             this.grabkey = false;
+        }
+
+        if (this.heldblocks.length > 0) {
+            this.currentSprite = (this.heldblocks[0]-2)%4+1;
+        } else {
+            this.currentSprite = 0;
         }
 
         if (kb.check(kb.F) && !this.throwkey) {
@@ -79,7 +90,8 @@ define(function(require) {
 
     Player.prototype.render = function(ctx) {
         ctx.fillStyle = '#FFF';
-        ctx.fillRect(this.x, this.y, 16, 16);
+
+        this.playertiles.renderTile(ctx, this.currentSprite, this.x, this.y);
 
         var world = this.engine.world;
         var x = this.x + 7;
