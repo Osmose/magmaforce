4f2ea62df49c36802a8f59cc4b47267989dbf844
Michael Kelly <mkelly@mozilla.com>
2012-11-03 12:20:42 -0400

    Initial customization.

--- js/game/game.js
+++ js/game/game.js
@@ -4,28 +4,19 @@ define(function(require) {
     var Graphic = require('flux/graphics/graphic');
     var Loader = require('flux/resources/loader');
 
-    // Create a new resource loader.
-    var loader = new Loader();
+    var Player = require('game/player');
 
-    // REGISTER RESOURCES TO LOAD HERE
-    loader.register('firefox', 'img/firefox.png', 'image');
-
-    // Callback run once all resources have been loaded.
-    loader.loadAll().done(function() {
         // Initialize engine.
-        var engine = new Engine(160, 144, 3);
-        engine.bg_color = '#FFFF8B';
+        var COLS = 12;
+        var ROWS = 12;
+        var engine = new Engine(COLS * 8, ROWS * 8, 3);
+        engine.bg_color = '#000';
+        engine.cols = COLS;
+        engine.rows = ROWS;
 
-        // ADD INITIAL STATE (entities, worlds, etc) HERE
-        function Player(x, y) {
-            Entity.call(this, x, y);
-            this.graphic = new Graphic(loader.get('firefox'));
-        }
-        Player.prototype = Object.create(Entity.prototype);
-        engine.addEntity(new Player(10, 10));
+        engine.addEntity(new Player(0, 11 * 8));
 
         // Append canvas to screen and start the engine!
         document.querySelector('#game').appendChild(engine.canvas);
         engine.start();
-    });
 });
---
+++ js/game/player.js
@@ -0,0 +1,33 @@
+define(function(require) {
+    var Entity = require('flux/entity');
+
+    function Player(x, y) {
+        Entity.call(this, x, y);
+        this.col = 0;
+    }
+    Player.prototype = Object.create(Entity.prototype);
+
+    Player.prototype.tick = function() {
+        Entity.prototype.tick.call(this);
+        var kb = this.engine.kb;
+
+        var dx = 0;
+        if (kb.check(kb.RIGHT)) {
+            dx += 1;
+        }
+        if (kb.check(kb.LEFT)) {
+            dx -= 1;
+        }
+
+        this.col += dx;
+        this.col = this.col % this.engine.cols;
+        this.x = this.col * 8;
+    };
+
+    Player.prototype.render = function(ctx) {
+        ctx.fillStyle = '#FFF';
+        ctx.fillRect(this.x, this.y, 8, 8);
+    };
+
+    return Player;
+});
